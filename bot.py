24644f6b88d2309a6656ec2f7f594b07bf75d74d
nikosing135 <nikosing135@gmail.com>
2025-07-20 01:37:43 +0330

    Update bot.py

--- bot.py
+++ bot.py
@@ -74,7 +74,6 @@ def home():
 
 import os
 
-
 if __name__ == '__main__':
-    port = int(os.environ.get("PORT", 5000))
+    port = int(os.environ.get('PORT', 5000))
     app.run(host='0.0.0.0', port=port)
