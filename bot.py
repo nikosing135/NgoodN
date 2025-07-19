28174130bd801cb1ada470ed9e163a4f87694322
nikosing135 <nikosing135@gmail.com>
2025-07-20 01:33:32 +0330

    Update bot.py

--- bot.py
+++ bot.py
@@ -72,6 +72,9 @@ def send_news_to_all():
 def home():
     return 'Bot is running.'
 
+import os
+
 
 if __name__ == '__main__':
-    app.run()
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host='0.0.0.0', port=port)
